bbe8a4a9430a8e4d4463cf32bc92aba02f7cb253
costa <viniciuscoscar@gmail.com>
2026-04-22 18:49:52 +0100

    Just translated the comments to english

--- resize.py
+++ resize.py
@@ -5,17 +5,16 @@ import sys
 fname_in = sys.argv[1]
 fname_out = sys.argv[2]
 
-# Abrir a imagem original
+# Open original image
 img = Image.open(fname_in)
 
 w = int(input("New width: "))
 h = int(input("New height: "))
 
-# Redimensionar para WxH
-# img_resized = img.resize((w, h), Image.Resampling.LANCZOS)
+# Resize to new width and height
 img_resized = img.resize((w, h))
 
-# Salvar a nova imagem
+# Save new image with the name of the output file
 img_resized.save(fname_out)
 
 print(f"{fname_out} successfully created")
